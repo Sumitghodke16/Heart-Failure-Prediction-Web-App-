e53034452cbf028e4cb49339180fc088534a3646
Sumit Naresh Ghodke <sumitghodke116@gmail.com>
2026-04-27 13:26:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import joblib
+import numpy as np
+import sys
+
+# Load the trained model and scaler
+heart_model = joblib.load('heart_failure_model.pkl')
+heart_scaler = joblib.load('scaler.pkl')
+
+st.title("Heart Failure Prediction Application")
+
+st.image("https://img.freepik.com/free-vector/detailed-vector-illustration-human-heart_1308-180437.jpg",
+            caption="Heart Failure Prediction",
+            width=250)
+
+age = st.slider("Age", 20, 95, 60)
+anaemia = st.selectbox("Anaemia", [0, 1])
+creatinine = st.number_input("Creatinine Phosphokinase", min_value=0, max_value=10000, value=500)
+diabetes = st.radio("Diabetes", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
+ef = st.number_input("Ejection Fraction", min_value=0, max_value=100, value=30)
+hbp = st.radio("High Blood Pressure", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
+platelets = st.number_input("Platelets", min_value=0, max_value=1000000, value=250000)
+serum_creatinine = st.number_input("Serum Creatinine", min_value=0.0, max_value=20.0, value=1.0)
+serium_sodium = st.number_input("Serum Sodium", min_value=0, max_value=200, value=135)
+sex = st.selectbox("Sex (1=Male and 0=Female)", [0, 1])
+smoking = st.selectbox("Smoking", [0, 1], format_func=lambda x: "Yes" if x == 1 else "No")
+time = st.number_input("Time (Follow-up period in days)", min_value=0, max_value=300, value=100)
+
+if st.button("Predict"):
+    heart_input = np.array([[age, anaemia, creatinine, diabetes, ef, hbp, platelets, serum_creatinine, 
+                             serium_sodium, sex, smoking, time]])
+    scaled = heart_scaler.transform(heart_input)
+    result = heart_model.predict(scaled)
+    if result[0] == 1:
+        st.success("Heart Failure Event")
+    else:
+        st.success("He is safe")
